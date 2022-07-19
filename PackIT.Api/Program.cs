64574c895b7afd5f848cebf9814c4a1711062a86
Ros Sokcheanith <sokcheanith1031@hotmail.com>
2022-07-19 14:12:40 +0700

    a

--- PackIT.Api/Program.cs
+++ PackIT.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddShared();
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+//builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
